e628733f3e1e9f239d35dc720b78054c17162447
Petr <162743671+petuls@users.noreply.github.com>
2025-01-09 09:08:01 +0100

    Initial Commit

---
+++ toBeDefined/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  toBeDefined
+//
+//  Created by Petr Stupka on 09.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ toBeDefined/toBeDefinedApp.swift
@@ -0,0 +1,17 @@
+//
+//  toBeDefinedApp.swift
+//  toBeDefined
+//
+//  Created by Petr Stupka on 09.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct toBeDefinedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
